b253d2f8447a6b319bad6ae5c2d7efe83e68cfb4
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-22 16:28:37 +0200

    implement component

--- src/BusinessAccount.jsx
+++ src/BusinessAccount.jsx
@@ -1,9 +1,11 @@
+import AllBenefits from './all-benefits/Allbenefits';
 import Banner from './banner-business-account/header';
 
 const BusinessAccount = () => {
   return (
     <>
       <Banner />
+      <AllBenefits />
     </>
   );
 };
