23b4d9fe055f24d426253fc40bf7dbf57f9f8b6b
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-22 16:27:25 +0200

    create main components

---
+++ src/all-benefits/style.ts
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+
+const SectionContainer = styled.section``;
+export {SectionContainer};
+
+const DivContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`;
+export {DivContainer};
+
+const TextContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export {TextContent};
+
+const TitleFirst = styled.p`
+  text-transform: uppercase;
+`;
+export {TitleFirst};
+
+const TitleSecond = styled.h2``;
+export {TitleSecond};
+
+const TitleThird = styled.p``;
+export {TitleThird};
+
+const CardsContent = styled.div``;
+export {CardsContent}
+
+
