54b7edf1af859af33c62d6dc5dc3edb81f5443c9
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-22 18:22:26 +0200

    add  component HowItWorks

--- src/BusinessAccount.jsx
+++ src/BusinessAccount.jsx
@@ -1,11 +1,12 @@
 import AllBenefits from './all-benefits/Allbenefits';
 import Banner from './banner-business-account/header';
-
+import HowItWorks from './how-it-works/HowItWorks';
 const BusinessAccount = () => {
   return (
     <>
       <Banner />
       <AllBenefits />
+      <HowItWorks />
     </>
   );
 };
