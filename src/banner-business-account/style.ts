6d688b69f327ec046966aaa172c040d2f7e2b99d
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-22 12:58:17 +0200

    create components to banner

---
+++ src/banner-business-account/style.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+const SectionContainer = styled.section`
+`;
+export { SectionContainer };
+
+const DivContainer = styled.div`
+`;
+export { DivContainer };
+
+const TitleContent = styled.h1`
+  font-size: 3rem;
+`;
+export { TitleContent };
+
+const SubTitle = styled.p`
+  font-size: 1.5rem;
+`;
+export { SubTitle };
+
+const HomeButton = styled.button`
+    margin: 2rem 0;
+    width: 10rem;
+    font-family: Moderat,sans-serif;
+    background-color: #195ab4;
+    color: #fff;
+    font-weight: 400;
+    font-size: 15px;
+    padding: 10px;
+    line-height: 16px;
+    cursor: pointer;
+    border: none;
+    border-color: transparent;
+    border-style: solid;
+    border-radius: 4px;
+    outline: none;
+    align-items: center;
+    justify-content: center;
+    transition-property: all;
+    transition-duration: .15s;
+`
+export {HomeButton};
