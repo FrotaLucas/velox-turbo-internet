bb13a3c2253c8283635a83dd31a46d8cbdcd3c97
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-22 17:49:58 +0200

    create button and complete TextContent, TitleFirst, TitleSecond

--- src/all-benefits/style.ts
+++ src/all-benefits/style.ts
@@ -9,9 +9,11 @@ const DivContainer = styled.div`
   margin: 0 auto;
   display: flex;
   padding-top: 5rem;
+  gap: 5rem;
 
   @media (max-width: 1024px){
   flex-direction: column;
+  gap: 1rem;
   }
 `;
 export {DivContainer};
@@ -19,15 +21,25 @@ export {DivContainer};
 const TextContent = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 5rem 0;
+
+  @media (max-width: 1024px){
+    padding: unset;
+  }
 `;
 export {TextContent};
 
 const TitleFirst = styled.p`
   text-transform: uppercase;
+  line-height: 125%;
 `;
 export {TitleFirst};
 
-const TitleSecond = styled.h2``;
+const TitleSecond = styled.h2`
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 125%;
+`;
 export {TitleSecond};
 
 const TitleThird = styled.p``;
@@ -48,5 +60,28 @@ const CardsContent = styled.div`
   }
 `;
 export {CardsContent}
+
+const BenefitsButton = styled.button`
+    margin: 2rem 0;
+    width: 10rem;
+    font-family: Moderat,sans-serif;
+    background-color: #195ab4;
+    color: #fff;
+    font-weight: 400;
+    font-size: 15px;
+    padding: 10px;
+    line-height: 16px;
+    cursor: pointer;
+    border: none;
+    border-color: transparent;
+    border-style: solid;
+    border-radius: 4px;
+    outline: none;
+    align-items: center;
+    justify-content: center;
+    transition-property: all;
+    transition-duration: .15s;
+`
+export {BenefitsButton};
 
 
