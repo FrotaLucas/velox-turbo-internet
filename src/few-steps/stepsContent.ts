c6deb038b93802880c66fb79233a402597bee762
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-23 14:25:55 +0200

    array with Line content

---
+++ src/few-steps/stepsContent.ts
@@ -0,0 +1,16 @@
+const stepsContent = [
+  {
+    content: 'Open your account'
+  },
+  {
+    content: 'Authorization to send E-mail'
+  },
+  {
+    content: 'Choose which internet suit you better'
+  },
+  {
+    content: 'Ready'
+  },
+]
+
+export default stepsContent
