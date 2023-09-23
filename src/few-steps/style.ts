2a24cc45e21d696fc4de123614d4f4cd1fa25f76
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-23 14:26:38 +0200

    create html structure of FewSteps

---
+++ src/few-steps/style.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+
+const SectionContainer = styled.section`
+  background-color:  #e6e7ea;;
+`;
+export {SectionContainer};
+
+const DivContainer = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  padding: 5rem 0;
+
+  @media (max-width: 1024px){
+  }
+`;
+export {DivContainer};
+
+const Title = styled.p`
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 125%;
+`;
+export {Title};
+
+const TextContainer = styled.div``;
+export {TextContainer};
+
+const ItensContainer = styled.div`
+  > div {
+    display: flex;
+    align-items: baseline;
+    gap: 1rem;
+  }
+`;
+export {ItensContainer}
+
+
