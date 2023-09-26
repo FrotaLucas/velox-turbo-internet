d42087b7519507d8ec35bc89b080f94d0a43cea9
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-26 13:27:03 +0200

    complement info of array stepsContent

--- src/few-steps/stepsContent.ts
+++ src/few-steps/stepsContent.ts
@@ -1,15 +1,23 @@
 const stepsContent = [
   {
-    content: 'Open your account'
+    title: 'Open your account',
+    imgTitle: 'Register your company via the Velox platform available in website or app.',
+    img: 'step1'
   },
   {
-    content: 'Authorization to send E-mail'
+    title: 'Authorization to send E-mail',
+    imgTitle: 'Authorize consultation of your adress',
+    img: 'step2'
   },
   {
-    content: 'Choose which internet suit you better'
+    title: 'Choose which internet suit you better',
+    imgTitle: 'Check the prices of our plans that are available and select what fit you better',
+    img: 'step3'
   },
   {
-    content: 'Ready'
+    title: 'Ready',
+    imgTitle: "You don't have to wait until the expiration date to receive your credit card sales",
+    img: 'step4'
   },
 ]
 
