3e249442d596469f9820956eb614434a7a389669
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-27 15:37:55 +0200

    implement component TItle

---
+++ src/questions/questions.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Title } from "./style";
+
+
+const Questions: React.FC = ()=> {
+
+  return(
+    <div>
+        <Title> Questions ?</Title>
+    </div>
+  )
+}
+
+export default Questions
