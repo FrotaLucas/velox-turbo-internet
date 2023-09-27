ecec96ebf217944645307f28e90c4aae27ca0719
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-27 15:04:28 +0200

    add property style at DotsProperties

--- src/few-steps/dots/dots.tsx
+++ src/few-steps/dots/dots.tsx
@@ -6,19 +6,24 @@ interface DotsProperties {
   onClick: () => void;
   indexNumber: number;
   content: string;
+  style?: React.CSSProperties;
 }
 
-const DotContainer: React.FC<DotsProperties> = ({ backGroundColor, onClick, indexNumber, content}) => {
+const DotContainer: React.FC<DotsProperties> = ({ backGroundColor, onClick, indexNumber, content, style}) => {
 
   return(
-    <>
+    <div style={{display: 'flex', gap: '1rem'}}>
+      <div style={{display: 'flex', marginBottom: '0.5rem'}}>
       <Dot toggle={backGroundColor} onClick={onClick}>
         {indexNumber}
       </Dot>
+      <span></span>
+      </div>
+
       <LineContent>
         {content}
       </LineContent>
-    </>
+    </div>
   )
 }
 
