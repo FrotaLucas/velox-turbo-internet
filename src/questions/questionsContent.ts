90bd3257d64f75634932006db8829f58c1019c33
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-27 15:34:10 +0200

    create file with questions

---
+++ src/questions/questionsContent.ts
@@ -0,0 +1,16 @@
+const questionsContent = [
+  {
+    title: 'How to open your Velox account',
+    content: 'Any pending will be communicated via email. Count on the support of our service time at any time to answer questions through our channels: (49) 145 3003-6299 (capitals and metropolitan regions), (11) 3383-3336 (via Whatsapp). The service can also be done by e-mail at costumerservice@velox.com.'
+  },
+  {
+    title: 'What are the necessary documents to open your account ?',
+    content: " To complete the registration of your company you must first accept and sign the terms and conditions. Then provide the articles of incorporation and the documentation of the company's legal representatives: Articles of Incorporation or Bylaws, identification of representatives such as ID or Drive License. If the person responsible for hiring is not a partner, it will be necessary to send the identification of the attorney-in-fact with the power of attorney."
+  },
+  {
+    title: 'How can I access the solutions of Velox ?',
+    content: 'If you are an entrepreneur and want to hire our financial solutions, start registering on our platform and open your Velox account. There, you will have access to our technology solutions.'
+  }
+]
+
+export default questionsContent;
