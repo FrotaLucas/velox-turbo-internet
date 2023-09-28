f7115453871fb13f9d09d2edd335e924baf116c1
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-28 23:04:50 +0200

    set type of variable index

--- src/questions/questions.tsx
+++ src/questions/questions.tsx
@@ -8,7 +8,7 @@ interface QuestionsProps {
 
 const Questions: React.FC<QuestionsProps> = ()=> {
 const [heights, setHeights] = React.useState<QuestionsProps["heightsType"]>([false, false,false]);
-function handleClick(index){
+function handleClick(index: number){
   const collapsed = [...heights];
   collapsed[index] = !collapsed[index];
   setHeights(collapsed);
@@ -20,8 +20,11 @@ function handleClick(index){
         {
           QuestionsContent.map((item,index) => 
             <QuestionItem key={index} trigger={heights[index]} onClick={()=> handleClick(index)}>
-              <div key={`title-${index}`}>
-                {item.title}
+              <div>
+                <div key={`title-${index}`}>
+                  {item.title}
+                </div>
+                <img src={`./public/svgs/${heights[index] ? 'arrow_up' : 'arrow_down'}.svg`}/>
               </div>
               <span key={`line-style-${index}`}></span>
               <div key={`content-${index}`}>
