49531b26c9dfa30753df8d8c4825d83242fb9147
FrotaLucas <lucasdiasfrota@gmail.com>
2023-09-28 22:42:25 +0200

    implement QuestionItem

--- src/questions/questions.tsx
+++ src/questions/questions.tsx
@@ -1,13 +1,37 @@
 import React from "react";
-import { Title } from "./style";
+import {DivContainer, QuestionItem, SectionContainer, Title } from "./style";
+import QuestionsContent from "./questionsContent";
 
+interface QuestionsProps {
+  heightsType: Array<boolean>;
+}
 
-const Questions: React.FC = ()=> {
-
+const Questions: React.FC<QuestionsProps> = ()=> {
+const [heights, setHeights] = React.useState<QuestionsProps["heightsType"]>([false, false,false]);
+function handleClick(index){
+  const collapsed = [...heights];
+  collapsed[index] = !collapsed[index];
+  setHeights(collapsed);
+}
   return(
-    <div>
+    <SectionContainer>
+      <DivContainer>
         <Title> Questions ?</Title>
-    </div>
+        {
+          QuestionsContent.map((item,index) => 
+            <QuestionItem key={index} trigger={heights[index]} onClick={()=> handleClick(index)}>
+              <div key={`title-${index}`}>
+                {item.title}
+              </div>
+              <span key={`line-style-${index}`}></span>
+              <div key={`content-${index}`}>
+                {item.content}
+              </div>
+            </QuestionItem>
+          )}
+        
+      </DivContainer>
+    </SectionContainer>
   )
 }
 
