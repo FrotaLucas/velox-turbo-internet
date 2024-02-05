d04a83c07a71d7f3e778a18a5ed7a92f04d65bc4
FrotaLucas <lucasdiasfrota@gmail.com>
2024-02-05 18:10:35 +0100

    reconstruct Modal arquitecture

--- src/all-benefits/modal/modal.tsx
+++ src/all-benefits/modal/modal.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Form, FormContent, InputField, ReturnButton } from './style';
+import { Overlay, FormContent, InputField, ReturnButton } from './style';
 
 interface ModalProps {
   isOpen: boolean;
@@ -11,48 +11,45 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose }) => {
   }, [isOpen]);
   return (
     <>
-      {isOpen && (
-        <Form>
-          <FormContent toggle={isOpen}>
-            <ReturnButton onClick={onClose}>
-              <img src={`svgs/chevron_left.svg`} />
-              Return
-            </ReturnButton>
+      <Overlay toggle={isOpen} />
+      <FormContent toggle={isOpen}>
+        <ReturnButton onClick={onClose}>
+          <img src={`svgs/chevron_left.svg`} />
+          Return
+        </ReturnButton>
 
-            <h5>Make a Request</h5>
-            <h6>Fill out this form and our team will get in touch with you</h6>
-            <InputField>
-              <label>Name</label>
-              <input placeholder="Digite" />
-            </InputField>
-            <InputField>
-              <label>E-mail</label>
-              <input placeholder="Digite" />
-            </InputField>
-            <InputField>
-              <label>Cellphone</label>
-              <input placeholder="Digite" />
-            </InputField>
-            <InputField>
-              <label>E-mail</label>
-              <input placeholder="Digite" />
-            </InputField>
+        <h5>Make a Request</h5>
+        <h6>Fill out this form and our team will get in touch with you</h6>
+        <InputField>
+          <label>Name</label>
+          <input placeholder="enter" />
+        </InputField>
+        <InputField>
+          <label>E-mail</label>
+          <input placeholder="enter" />
+        </InputField>
+        <InputField>
+          <label>Cellphone</label>
+          <input placeholder="enter" />
+        </InputField>
+        <InputField>
+          <label>E-mail</label>
+          <input placeholder="enter" />
+        </InputField>
 
-            <InputField>
-              <label>Tax Identification</label>
-              <input placeholder="Digite" />
-            </InputField>
+        <InputField>
+          <label>Tax Identification</label>
+          <input placeholder="enter" />
+        </InputField>
 
-            <label> Revenue</label>
-            <select name="revenue-range" id="revenue">
-              <option>Up to € 50.000 </option>
-              <option>€50.000 to €500.000</option>
-              <option>€500.000 to €1.000.000</option>
-              <option>€1.000.000 to 10.000.000</option>
-            </select>
-          </FormContent>
-        </Form>
-      )}
+        <label> Revenue</label>
+        <select name="revenue-range" id="revenue">
+          <option>Up to € 50.000 </option>
+          <option>€50.000 to €500.000</option>
+          <option>€500.000 to €1.000.000</option>
+          <option>€1.000.000 to 10.000.000</option>
+        </select>
+      </FormContent>
     </>
   );
 };
